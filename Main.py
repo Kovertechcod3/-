01cbccd143432082ce1c90bc8f20c1b8b9c52af7
Kovertechcod3 <144227649+Kovertechcod3@users.noreply.github.com>
2023-11-20 11:10:17 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,9 +6,9 @@ import click
 from bs4 import BeautifulSoup
 import uuid
 
-output_dir = "search_results"
-search_engine_url = "https://www.google.com/search"
-language_param = "&lr=lang_en"
+OUTPUT_DIR = "search_results"
+SEARCH_ENGINE_URL = "https://www.google.com/search"
+LANGUAGE_PARAM = "&lr=lang_en"
 
 def create_output_directory(output_dir):
     os.makedirs(output_dir, exist_ok=True)
@@ -33,7 +33,7 @@ def extract_url_from_search_result(result):
 def validate_http_response(response):
     response.raise_for_status()
 
-def handle_file_io_errors(query, page, error):
+def handle_file_io_error(query, page, error):
     logging.error(f"Error occurred while writing to the file for query '{query}' on page {page}: {error}")
 
 def generate_search_summary(result):
@@ -41,230 +41,78 @@ def generate_search_summary(result):
     # Generate summary logic here
     return summary
 
-def apply_pagination(query, page):
-    search_url = search_engine_url + "?q=" + query.replace(" ", "+") + language_param + f"&start={str((page-1)*10)}"
-    response = requests.get(search_url)
-    validate_http_response(response)
-    search_results = extract_search_results(response.text)
-    filtered_results = filter_search_results(search_results, "dumps")
-    save_to_file(filtered_results)
-    summaries = [generate_search_summary(result) for result in filtered_results]
-    time.sleep(1)
-
-def parse_search_parameters(query):
-    search_url = search_engine_url + "?q=" + query.replace(" ", "+") + language_param
-    response = requests.get(search_url)
-    validate_http_response(response)
-    search_results = extract_search_results(response.text)
-    filtered_results = filter_search_results(search_results, "dumps")
-    save_to_file(filtered_results)
-    summaries = [generate_search_summary(result) for result in filtered_results]
-    return search_results
-
-def apply_search_filters(search_results, criteria):
-    filtered_results = []
-    for result in search_results:
-        if criteria in result["title"].lower():
-            filtered_results.append(result)
-    return filtered_results
-
-def log_error_details(query, page, error):
-    logging.error(f"Error occurred while processing query '{query}' on page {page}: {error}")
-
-def handle_unknown_errors(query, error):
-    logging.error(f"Unknown error occurred while processing query '{query}': {error}")
-
-def add_delay_between_requests():
-    time.sleep(1)
-
 def extract_search_results(html_content):
     soup = BeautifulSoup(html_content, "html.parser")
     result_divs = soup.find_all("div", class_="g")
     search_results = []
     for div in result_divs:
-        result = {}
-        result["title"] = extract_title_from_search_result(div)
-        result["url"] = extract_url_from_search_result(div)
+        result = {
+            "title": extract_title_from_search_result(div),
+            "url": extract_url_from_search_result(div)
+        }
         search_results.append(result)
     return search_results
 
-def save_to_file(search_results):
+def save_search_results_to_file(search_results):
     filename = generate_output_filename()
-    output_path = os.path.join(output_dir, filename)
+    output_path = os.path.join(OUTPUT_DIR, filename)
     with open(output_path, "w", encoding="utf-8") as file:
         for result in search_results:
             write_search_result_to_file(file, result)
-    logging.info(f"Search Results saved to: {output_path}")
-
-def perform_search(query):
-    try:
-        search_results = parse_search_parameters(query)
-        fetch_multiple_pages(query)
-        add_delay_between_requests()
-    except requests.exceptions.RequestException as e:
-        log_error_details(query, 1, e)
-    except IOError as e:
-        handle_file_io_errors(query, 1, e)
-    except Exception as e:
-        handle_unknown_errors(query, e)
-
-def fetch_multiple_pages(query):
-    page = 1
-    while page <= 3:
-        try:
-            apply_pagination(query, page)
-            page += 1
-        except requests.exceptions.RequestException as e:
-            log_error_details(query, page, e)
-        except IOError as e:
-            handle_file_io_errors(query, page, e)
-        except Exception as e:
-            handle_unknown_errors(query, e)
-
-def filter_search_results(search_results, criteria):
-    filtered_results = []
-    for result in search_results:
-        if criteria in result["title"].lower():
-            filtered_results.append(result)
-    return filtered_results
-
-@click.command()
-@click.option("-o", "--output", type=str, default="search_results", help="Output directory for search results")
-@click.option("-v", "--verbose", is_flag=True, help="Enable verbose logging")
-@click.argument("queries", nargs=-Sure! Here's a refactored version of the code that separates the logic into smaller functions and improves code readability:
-
-```python
-import os
-import requests
-import time
-import logging
-import click
-from bs4 import BeautifulSoup
-import uuid
-
-output_dir = "search_results"
-search_engine_url = "https://www.google.com/search"
-language_param = "&lr=lang_en"
-
-def create_output_directory(output_dir):
-    os.makedirs(output_dir, exist_ok=True)
-
-def configure_logging():
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
-def generate_output_filename():
-    return str(uuid.uuid4()) + ".txt"
-
-def write_search_result_to_file(file, result):
-    file.write(f"Title: {result['title']}\n")
-    file.write(f"URL: {result['url']}\n")
-    file.write("------------------------\n")
-
-def extract_title_from_search_result(result):
-    return result.find("h3").text
-
-def extract_url_from_search_result(result):
-    return result.find("a")["href"]
-
-def validate_http_response(response):
-    response.raise_for_status()
-
-def handle_file_io_errors(query, page, error):
-    logging.error(f"Error occurred while writing to the file for query '{query}' on page {page}: {error}")
-
-def generate_search_summary(result):
-    summary = ""
-    # Generate summary logic here
-    return summary
+    logging.info(f"Search results saved to: {output_path}")
 
 def apply_pagination(query, page):
-    search_url = search_engine_url + "?q=" + query.replace(" ", "+") + language_param + f"&start={str((page-1)*10)}"
+    search_url = f"{SEARCH_ENGINE_URL}?q={query.replace(' ', '+')}{LANGUAGE_PARAM}&start={(page-1)*10}"
     response = requests.get(search_url)
     validate_http_response(response)
     search_results = extract_search_results(response.text)
-    filtered_results = filter_search_results(search_results, "dumps")
-    save_to_file(filtered_results)
+    filtered_results = [result for result in search_results if "dumps" in result["title"].lower()]
+    save_search_results_to_file(filtered_results)
     summaries = [generate_search_summary(result) for result in filtered_results]
     time.sleep(1)
-
-def parse_search_parameters(query):
-    search_url = search_engine_url + "?q=" + query.replace(" ", "+") + language_param
-    response = requests.get(search_url)
-    validate_http_response(response)
-    search_results = extract_search_results(response.text)
-    filtered_results = filter_search_results(search_results, "dumps")
-    save_to_file(filtered_results)
-    summaries = [generate_search_summary(result) for result in filtered_results]
-    return search_results
-
-def apply_search_filters(search_results, criteria):
-    filtered_results = []
-    for result in search_results:
-        if criteria in result["title"].lower():
-            filtered_results.append(result)
-    return filtered_results
-
-def log_error_details(query, page, error):
-    logging.error(f"Error occurred while processing query '{query}' on page {page}: {error}")
-
-def handle_unknown_errors(query, error):
-    logging.error(f"Unknown error occurred while processing query '{query}': {error}")
-
-def add_delay_between_requests():
-    time.sleep(1)
-
-def extract_search_results(html_content):
-    soup = BeautifulSoup(html_content, "html.parser")
-    result_divs = soup.find_all("div", class_="g")
-    search_results = []
-    for div in result_divs:
-        result = {}
-        result["title"] = extract_title_from_search_result(div)
-        result["url"] = extract_url_from_search_result(div)
-        search_results.append(result)
-    return search_results
-
-def save_to_file(search_results):
-    filename = generate_output_filename()
-    output_path = os.path.join(output_dir, filename)
-    with open(output_path, "w", encoding="utf-8") as file:
-        for result in search_results:
-            write_search_result_to_file(file, result)
-    logging.info(f"Search Results saved to: {output_path}")
-
-def perform_search(query):
-    try:
-        search_results = parse_search_parameters(query)
-        fetch_multiple_pages(query)
-        add_delay_between_requests()
-    except requests.exceptions.RequestException as e:
-        log_error_details(query, 1, e)
-    except IOError as e:
-        handle_file_io_errors(query, 1, e)
-    except Exception as e:
-        handle_unknown_errors(query, e)
-
-def fetch_multiple_pages(query):
-    page = 1
-    while page <= 3:
-        try:
-            apply_pagination(query, page)
-            page += 1
-        except requests.exceptions.RequestException as e:
-            log_error_details(query, page, e)
-        except IOError as e:
-            handle_file_io_errors(query, page, e)
-        except Exception as e:
-            handle_unknown_errors(query, e)
-
-def filter_search_results(search_results, criteria):
-    filtered_results = []
-    for result in search_results:
-        if criteria in result["title"].lower():
-            filtered_results.append(result)
-    return filtered_results
 
 @click.command()
 @click.option("-o", "--output", type=str, default="search_results", help="Output directory for search results")
-@click.option("-v", "--verbose", is_flag=True, help="Enable verbose logging")
-@click.argument("queries", nargs=-
+@click.argument("queries", nargs=-1, required=True)
+def main(output, queries):
+    global OUTPUT_DIR
+    OUTPUT_DIR = output
+    create_output_directory(OUTPUT_DIR)
+    configure_logging()
+
+    while True:
+        click.clear()
+        click.echo("=== Search Menu ===")
+        click.echo("1. Perform Single Search")
+        click.echo("2. Perform Multiple Searches")
+        click.echo("0. Exit")
+
+        choice = click.prompt("Enter your choice", type=int)
+
+        if choice == 1:
+            query = click.prompt("Enter your query")
+            perform_search(query)
+            click.echo("Search complete!")
+            click.pause()
+
+        elif choice == 2:
+            queries = click.prompt("Enter multiple queries (comma-separated)")
+            queries = queries.split(",")
+            for query in queries:
+                perform_search(query.strip())
+            click.echo("Search complete!")
+            click.pause()
+
+        elif choice == 0:
+            break
+
+        else:
+            click.echo("Invalid choice. Please try again.")
+
+def perform_search(query):
+    click.clear()
+    click.echo(f"Performing search for '{query}'...")
+    apply_pagination(query, 1)
+
+if __name__ == "__main__":
+    main()
