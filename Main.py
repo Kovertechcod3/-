8b059455ecc68b609d08ae6217491344ecaf40a4
Kovertechcod3 <144227649+Kovertechcod3@users.noreply.github.com>
2023-11-19 21:20:54 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,116 +1,147 @@
+import os
 import requests
-from bs4 import BeautifulSoup
-from concurrent.futures import ThreadPoolExecutor
 import time
-import csv
-import json
-import vulnscanner
-from urllib.parse import urljoin, urlparse
+import logging
+import click
+from bs4 import BeautifulSoup
+import uuid
 
-config = json.load(open('config.json'))
+output_dir = "search_results"
+search_engine_url = "https://www.google.com/search"
+language_param = "&lr=lang_en"
 
-base_url = config['target']
-timeout = config['timeout']
-threads = config['threads']
-output = config['output']
+def create_output_directory(output_dir):
+    os.makedirs(output_dir, exist_ok=True)
 
-results = []
+def configure_logging():
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-# Request handler with error catching
-def request(url):
-  try:
-    res = requests.get(url, timeout=timeout)
-  except requests.exceptions.RequestException as e:
-    print(f"Error on {url}: {e}")
-    return None
-  
-  return res
+def generate_output_filename():
+    return str(uuid.uuid4()) + ".txt"
 
+def write_search_result_to_file(file, result):
+    file.write(f"Title: {result['title']}\n")
+    file.write(f"URL: {result['url']}\n")
+    file.write("------------------------\n")
 
-# Extract and recursively requests links  
-def extract_links(url):
-  res = request(url)
-  if res is None:
-    return
+def extract_title_from_search_result(result):
+    return result.find("h3").text
 
-  links = BeautifulSoup(res.text, 'html.parser').find_all('a')
+def extract_url_from_search_result(result):
+    return result.find("a")["href"]
 
-  with ThreadPoolExecutor(max_workers=threads) as executor:
-    executor.map(request_link, links)
+def validate_http_response(response):
+    response.raise_for_status()
 
+def handle_file_io_errors(query, page, error):
+    logging.error(f"Error occurred while writing to the file for query '{query}' on page {page}: {error}")
 
-# Check individual link  
-def request_link(link):
-  href = link['href']
-  href = urljoin(base_url, href)
+def generate_search_summary(result):
+    summary = ""
+    # Generate summary logic here
+    return summary
 
-  check_response(request(href))
+def apply_pagination(query, page):
+    search_url = search_engine_url + "?q=" + query.replace(" ", "+") + language_param + f"&start={str((page-1)*10)}"
+    response = requests.get(search_url)
+    validate_http_response(response)
+    search_results = extract_search_results(response.text)
+    filtered_results = filter_search_results(search_results, "dumps")
+    save_to_file(filtered_results)
+    summaries = [generate_search_summary(result) for result in filtered_results]
+    time.sleep(1)
 
+def parse_search_parameters(query):
+    search_url = search_engine_url + "?q=" + query.replace(" ", "+") + language_param
+    response = requests.get(search_url)
+    validate_http_response(response)
+    search_results = extract_search_results(response.text)
+    filtered_results = filter_search_results(search_results, "dumps")
+    save_to_file(filtered_results)
+    summaries = [generate_search_summary(result) for result in filtered_results]
+    return search_results
 
-# Check response for clues
-def check_response(res):
-  if res is None:
-    return
+def apply_search_filters(search_results, criteria):
+    filtered_results = []
+    for result in search_results:
+        if criteria in result["title"].lower():
+            filtered_results.append(result)
+    return filtered_results
 
-  print(f"Checking {res.url}")
+def log_error_details(query, page, error):
+    logging.error(f"Error occurred while processing query '{query}' on page {page}: {error}")
 
-  check_headers(res)
-  check_content(res.text)
+def handle_unknown_errors(query, error):
+    logging.error(f"Unknown error occurred while processing query '{query}': {error}")
 
-  vulnscanner.scan_response(res)
+def add_delay_between_requests():
+    time.sleep(1)
 
-  time.sleep(1)
+def extract_search_results(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    result_divs = soup.find_all("div", class_="g")
+    search_results = []
+    for div in result_divs:
+        result = {}
+        result["title"] = extract_title_from_search_result(div)
+        result["url"] = extract_url_from_search_result(div)
+        search_results.append(result)
+    return search_results
 
+def save_to_file(search_results):
+    filename = generate_output_filename()
+    output_path = os.path.join(output_dir, filename)
+    with open(output_path, "w", encoding="utf-8") as file:
+        for result in search_results:
+            write_search_result_to_file(file, result)
+    logging.info(f"Search Results saved to: {output_path}")
 
-# Header and content checks  
-def check_headers(res):
-  if "password" in res.headers:
-    print("Header infoleak")
+def perform_search(query):
+    try:
+        search_results = parse_search_parameters(query)
+        fetch_multiple_pages(query)
+        add_delay_between_requests()
+    except requests.exceptions.RequestException as e:
+        log_error_details(query, 1, e)
+    except IOError as e:
+        handle_file_io_errors(query, 1, e)
+    except Exception as e:
+        handle_unknown_errors(query, e)
 
-  if "debug" in res.text:
-     print("Debug code")
+def fetch_multiple_pages(query):
+    page = 1
+    while page <= 3:
+        try:
+            apply_pagination(query, page)
+            page += 1
+        except requests.exceptions.RequestException as e:
+            log_error_details(query, page, e)
+        except IOError as e:
+            handle_file_io_errors(query, page, e)
+        except Exception as e:
+            handle_unknown_errors(query, e)
 
+def filter_search_results(search_results, criteria):
+    filtered_results = []
+    for result in search_results:
+        if criteria in result["title"].lower():
+            filtered_results.append(result)
+    return filtered_results
 
-# Check default paths
-def check_defaults():
-  paths = ['/admin','/config'] 
+@click.command()
+@click.option("-o", "--output", type=str, default="search_results", help="Output directory for search results")
+@click.option("-v", "--verbose", is_flag=True, help="Enable verbose logging")
+@click.argument("queries", nargs=-1)
+def main(output, verbose, queries):
+    global output_dir
+    output_dir = output
+    if verbose:
+        configure_logging()
 
-  for path in paths:
-    check_response(request(urljoin(base_url, path)))
+    create_output_directory(output_dir)
 
+    for query in queries:
+        perform_search(query)
 
-# Parameter tester  
-def fuzz_params(path):
-  payloads = ['1','../','"%20']
-
-  for payload in payloads:
-     check_response(request(path + payload))
-
-
-# Output results
-def output_results():
-  if output == 'csv':
-    with open('results.csv', 'w') as f:
-      writer = csv.writer(f)
-
-      for result in results:
-         writer.writerow(result)
-
-  elif output == 'json':
-    with open('results.json', 'w') as f:
-      json.dump(results, f)
-
-
-# Main driver
-def run():
-  extract_links(base_url) 
-  check_defaults()
-
-  parsed = urlparse(base_url)
-  fuzz_params(parsed.path)
-
-  output_results()
-
-
-if __name__ == '__main__':
-  run()
+if __name__ == "__main__":
+    main()
